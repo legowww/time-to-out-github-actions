313ae13b9a6ba8ff6814528c04cf3c52405841db
PHJ <legowww@gmail.com>
2023-01-14 03:15:45 +0900

    bug fix 시도 3

--- src/test/java/com/quadint/app/TtoAppApplicationTests.java
+++ src/test/java/com/quadint/app/TtoAppApplicationTests.java
@@ -3,7 +3,7 @@ package com.quadint.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TtoAppApplicationTests {
 
 	@Test
